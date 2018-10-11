b42aab75dc7ab1318bad83addf44947c6fe257a7
Alice Grigorjan <Alice.Grigorjan@Student.Reutlingen-University.de>
2018-10-11 17:07:03 +0200

    Implemented usernames, timestamp and user entered/left chat room

--- index.js
+++ index.js
@@ -3,15 +3,42 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+let userSocketsMap = new Map();
+
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    let json = {username: socket.username, message: msg, timestamp: Date.now()};
+    io.emit('chat message', json);
+  });
+
+  socket.on('add user', function(msg){
+    socket.username = msg;
+    userSocketsMap.set(socket.username, socket);
+    console.log('User '+socket.username+' entered the chat');
+    let json = {username: socket.username, timestamp: Date.now()};
+    io.emit('add user', json);
+  });
+
+  socket.on('list users', function(){
+   let usernames =  Array.from(userSocketsMap.keys());
+   socket.send('list users', usernames);
+  });
+
+  socket.on('disconnect', function(){
+    userSocketsMap.delete(socket.username);
+    console.log('User '+socket.username+' left the chat');
+    let json = {username: socket.username, timestamp: Date.now()};
+    io.emit('user left', json);
   });
 });
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
