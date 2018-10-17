4956c2393f8f4d4fefb11e8cb5a50a79559b41ea
mertag96 <Merve.Tagmut@student.reutlingen-university.de>
2018-10-17 15:18:07 +0200

    new design, registerpage, updated readme

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# A simple chat server
+#  Chat Group O
 
-This is the source code for a very simple chat example, based on
-the [Getting Started](http://socket.io/get-started/chat/) guide
-of the Socket.IO website, which has been extended by us. Registering with usernames, private chatting and sending multimedia files is now possible.
+This is the source code for a very simple chat.
 
-After cloning this project, run ```npm install``` and afterwards ```npm run start```. The chat server can be reached under ```localhost:3000```.
+**Functionalities**
+- Registering with usernames
+- Private chatting on command ```\w recipient message```
+- Listing online users on command ```\list``` 
+- Sending of multimedia files via private message or broadcast
+- Status messages if a user enters or leaves the chat
 
+**Getting Started**
+
+Run ```npm install``` and afterwards run ```npm run start```. 
+The chat server can be reached under ```localhost:3000```.
+
+**Authors**
+
+- Alice Grigorjan - [AliceGrigorjan](https://github.com/AliceGrigorjan)
+- Merve Tagmut - [mertag96](https://github.com/mertag96)
--- index.js
+++ index.js
@@ -1,62 +1,101 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var express = require('express'),
+    index = express(),
+    server = require('http').createServer(index),
+    io = require('socket.io').listen(server),
+    users = {},
+    port = 3000;
 
-let userSocketsMap = new Map();
 
-
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+server.listen(port, function() {
+    console.log('listening on *: ' + port);
 });
 
-io.on('connection', function(socket){
+//routing
+index.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
 
-  socket.on('chat message', function(msg){
-    let json = {username: socket.username, message: msg, timestamp: Date.now()};
-    io.emit('chat message', json);
-  });
+io.sockets.on('connection', function(socket) {
+    socket.on('new user', function(data, callback) {
+        if (data in users) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            users[socket.nickname] = socket;
+            updateNicknames();
+        }
+    });
 
-  socket.on('add user', function(msg){
-    if (userSocketsMap.has(msg)){
-      socket.emit('username taken', 'Username already taken, please reload the page.')
-      socket.disconnect();
-    }else{
-      socket.username = msg;
-      userSocketsMap.set(socket.username, socket);
-      console.log('User '+socket.username+' entered the chat');
-      let json = {username: socket.username, timestamp: Date.now()};
-      io.emit('add user', json);
+    function updateNicknames() {
+        io.sockets.emit('usernames', Object.keys(users));
     }
-  });
 
-  socket.on('user list', function(){
-   let usernames =  Array.from(userSocketsMap.keys());
-   console.log(usernames);
-   socket.emit('user list', usernames);
-  });
+    socket.on('send message', function(data, callback) {
+        var msg = data.trim();
+        if (msg.substring(0, 3) === '\\w ') {
+            msg = msg.substr(3);
+            var ind = msg.indexOf(' ');
+            if (ind !== -1) {
+                var name = msg.substring(0, ind);
+                var msg = msg.substring(ind + 1);
+                if (name in users) {
+                    socket.emit('whisper', {
+                        recipient: name,
+                        msg: msg,
+                        nick: socket.nickname,
+                        timestamp: new Date()
+                    });
+                    users[name].emit('whisper', {
+                        recipient: name,
+                        msg: msg,
+                        nick: socket.nickname,
+                        timestamp: new Date()
+                    });
+                    console.log('Whisper!');
+                } else {
+                    callback('error! enter a valid user');
+                }
+            } else {
+                callback('Failure: Please enter your message to whisper! ')
+            }
+        } else if (msg.substring(0, 5) === '\\ list') {
+            msg = msg.substr(5);
+            var ind = msg.indexOf(' ');
+            if (ind == -1) {
+                console.log('Show Users!')
+                socket.emit('list', Object.keys(users));
+            } else {
+                callback('Error! Please enter a correct command to show the online users')
+            }
+        } else {
+            io.sockets.emit('new message', {
+                msg: msg,
+                nick: socket.nickname,
+                timestamp: new Date()
+            });
+        }
+    });
 
-  socket.on('private message', function(msg){
-    let messagearray = msg.split(' ');
-    let recipient = messagearray[0].replace('@', '');
-    let sender = socket.username;
-    let timestamp = Date.now();
-    let recipientsocket = userSocketsMap.get(recipient);
-    let json = {sender: sender, timestamp: timestamp, recipient: recipient, message: msg};
-    recipientsocket.emit('private message', json);
+    socket.on('enter user', function(msg) {
+        socket.nickname = msg;
+        console.log('User ' + socket.nickname + ' entered the chat!');
+        let json = {
+            nickname: socket.nickname,
+            timestamp: new Date()
+        };
+        io.emit('enter user', json);
+    });
 
-  });
-
-  socket.on('disconnect', function(){
-    userSocketsMap.delete(socket.username);
-    console.log('User '+socket.username+' left the chat');
-    let json = {username: socket.username, timestamp: Date.now()};
-    io.emit('user left', json);
-  });
-});
-
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+    socket.on('disconnect', function() {
+        if (!socket.nickname) return;
+        delete users[socket.nickname];
+        console.log('User ' + socket.nickname + ' left the chat!');
+        updateNicknames();
+        let json = {
+            nickname: socket.nickname,
+            timestamp: new Date()
+        };
+        io.emit('user left', json);
+    });
 });
